20dbd4570eb7d3b9b1bab497755c4c30a425e970
ravipazl <ravikumar.r@pazl.info>
2025-12-30 17:48:21 +0530

    new updated code for the image upload

--- src/js/vue/config.js
+++ src/js/vue/config.js
@@ -1,3 +1,3 @@
 export const DOMAIN = 'https://worksuite-new.test';
 export const API_URL = `${ DOMAIN }/api/v1`;
-export const DOMAIN_LOGO = '/images/worksuite-logo.png';
+export const DOMAIN_LOGO = '/images/skalelit-logo.png';
